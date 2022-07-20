e189ae4b5bd552386b266832295c0511c9247aa6
akash <akashkumarrana1530@gmail.com>
2022-07-20 23:06:36 +0530

    23

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
 import expressAsyncHandler from 'express-async-handler';
-import User from '../models/userModel.js';
+import User from '../models/useModel.js';
 import { generateToken } from '../utils.js';
 
 const userRouter = express.Router();
